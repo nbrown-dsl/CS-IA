bfa0dc455c8f13ea56144f7cb77310f24381cb64
Tangeni Idhenga <tangeni.idhenga@gmail.com>
2021-03-21 19:59:56 +0000

    Q

--- About_page.py
+++ About_page.py
@@ -7,15 +7,17 @@ root.geometry("1000x500")
 
 #messagebox
 
-def popup():
+
+
+def aboutinfo():
     messagebox.showinfo("About the application", "This application was produced for...")
 
-Button(root, text="About the application", command=popup).pack(pady=30)
+Button(root, text="About the application", command=aboutinfo).pack(pady=30)
 
 
-def popup():
+def futureinfo():
     messagebox.showinfo("Updates and Future plans", "This application was produced for...")
 
-Button(root, text="Updates and Future plans", command=popup).pack(pady=30)
+Button(root, text="Updates and Future plans", command=futureinfo).pack(pady=30)
 
 root.mainloop()
--- Class_page.py
+++ Class_page.py
@@ -1,16 +1,31 @@
 from tkinter import * 
-from .User_page import *
+
 
 root = Tk()
 root.title("Class page")
 root.geometry("1000x500")
 
+Classes = [
+"User 1",
+"User 2",
+"User 3"
 
-list = IntVar()
+] 
 
-user_1 = Radiobutton(root, text="Math", variable=v, value=1)
-user_2 = Radiobutton(root, text="Math", variable=v, value=1)
-user_3 = Radiobutton(root, text="Math", variable=v, value=1)
+master = Tk()
+
+variable = StringVar(master)
+variable.set(Classes[0]) 
+
+w = OptionMenu(master, variable, *Classes)
+w.pack()
+
+
+
+list_1 = IntVar()
+
+user_1 = Radiobutton(root, text="Math", variable=list_1, value=1).pack()
+user_2 = Radiobutton(root, text="English", variable=list_1, value=2).pack()
 
 
 root.mainloop()
